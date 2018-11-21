02e9d41e2ae97ff91dd17257abbd06be87a50ed6
Carell Hamil <ayannahamil@gmail.com>
2018-11-21 19:45:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 //line of people, and a new peron's name and the returns a welcome
 //message including the new person's position in line
 const katzDeli = [];
-
+var i = 0;
 function takeANumber(currentLine, newPerson){
-
+  currentLine.push(newPerson);
+  i++;
+  console.log(`Welcome, ${newPerson}. You are number ${i} in line.`)
 }
