aff458072898de94e715b17fe8cabc4e7c2126ae
Carell Hamil <ayannahamil@gmail.com>
2018-11-21 20:42:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,72 @@
 //create function takeANumber which accepts the current
 //line of people, and a new peron's name and the returns a welcome
 //message including the new person's position in line
+//declare empty array for the deli line
+const katzDeliLine = [];
+
+//initialize counter variable
 var i = 0;
-function takeANumber(currentLine, newPerson){
-  currentLine = currentLine.push(newPerson);
+
+//declare function for taking a number
+//parameters are the currentLine array and the new person who is taking a number
+//this will populate the deli line array
+function takeANumber(deliLine, newPerson){
+  deliLine = deliLine.push(newPerson);
   i++;
   console.log(`Welcome, ${newPerson}. You are number ${i} in line.`)
 }
 
-function nowServing(currentLine) {
-	if (currentLine.length === 0){
-		console.log(`The line is currently empty.`);
+//test cases for populating the deli line array
+takeANumber(katzDeliLine, "Carell");
+takeANumber(katzDeliLine, "Andrew");
+takeANumber(katzDeliLine, "Anya");
+takeANumber(katzDeliLine, "Zara");
+takeANumber(katzDeliLine, "Kat");
+
+//test to check that the deli line array has been populated
+console.log(katzDeliLine);
+
+//declare function which takes the current deli line array as a parameter
+//returns the person who is now being served - should be the first element in the array
+//removes that element and returns the current deli line array
+
+function nowServing(deliLine) {
+	if (deliLine.length === 0){
+		console.log(`There is nobody waiting to be served!`);
 	}
 	else {
-		var personServing = currentLine.shift();
+		var personServing = deliLine.shift();
 		console.log(`Currently serving ${personServing}.`);
-		return currentLine;
+		return deliLine;
 	}
 }
+
+nowServing(katzDeliLine);
+nowServing(katzDeliLine);
+nowServing(katzDeliLine);
+nowServing(katzDeliLine);
+nowServing(katzDeliLine);
+
+console.log(katzDeliLine);
+
+//Build a function currentLine that accepts the current line of people and
+//returns the current line as a string; for example, if 'katzDeliLine' is currently
+//["Ada", "Grace"], currentLine(katzDeliLine) would return "The line is
+//currently: 1. Ada, 2. Grace". You don't have to use katzDeliLine as a variable or
+//parameter name in your function though, it's just an example of a variable that might
+//be passed to it. If there is nobody in line, it should return "The line is currently empty."
+
+function currentLine(deliLine) {
+	if (deliLine.length === 0 ){
+		console.log(`The line is currently empty.`);
+	}
+	else{
+		var myLine = `The line is currently: `;
+		for (var i = 0; i < deliLine.length; i++) {
+			myLine = myLine + `${[i+1]}. ${deliLine[i]} `;
+		}
+		console.log(myLine);
+	}
+}
+
+currentLine(katzDeliLine);
