a1e3532263f17f7c749b7c905d823c1527abf65e
Carell Hamil <ayannahamil@gmail.com>
2018-11-21 20:15:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //create function takeANumber which accepts the current
 //line of people, and a new peron's name and the returns a welcome
 //message including the new person's position in line
-const katzDeli = [];
 var i = 0;
 function takeANumber(currentLine, newPerson){
   currentLine.push(newPerson);
